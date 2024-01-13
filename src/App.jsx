643f6092810082a9f4a659a1f93fa588c97e6f7c
MD Musaddique Ali Erfan <erfanali3287@gmail.com>
2024-01-13 20:07:47 +0600

    updated gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Banner from "./components/Banner";
+import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <NavBar />
+        <Banner />
+        <Skills />
+        <Projects />
+      </div>
+    </>
+  );
 }
 
 export default App;
